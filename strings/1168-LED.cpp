30d2fd91aa85cba6ed8e4ea6338add8795f5a404
Augusto Caixeta <160799418+augustocaixeta@users.noreply.github.com>
2026-08-09 22:14:53 -0300

    1168-LED

---
+++ strings/1168-LED.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int const arr[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+
+    while (N--) {
+        char str[101];
+        scanf("%s", str);
+
+        int i = 0, leds = 0;
+        while (str[i]) {
+            leds += arr[str[i++] - '0'];
+        }
+
+        printf("%d leds\n", leds);
+    }
+
+    return 0;
+}
